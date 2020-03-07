c94e1be4c2431cd485e4a48c1dff9d45a83b8872
cuongnmhe140724 <57407849+cuongnmhe140724@users.noreply.github.com>
2020-03-07 23:06:51 +0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,6 +3,5 @@ function upDate(previewPic) {
 	document.getElementById('image').style.backgroundImage = 'url('+previewPic.src+')';
 }
 function unDo() {
-	document.getElementById('image').innerHTML = 'Hover over an image below to display here.';
 	document.getElementById('image').style.backgroundImage = 'url()';
 }
